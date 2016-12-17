ba8e163ebbbd2befa8255b0145c4e0cf81f96ba6
Caleb Fenton <calebjfenton@gmail.com>
2016-12-16 20:36:32 -0800

    Print number of rules when compiling

--- prep-release.py
+++ prep-release.py
@@ -8,11 +8,11 @@ from codecs import open
 rules_dir = 'apkid/rules/'
 compiled_rules_path = "%srules.yarc" % rules_dir
 
-print "[*] Converting Markdown README to reStructuredText ..."
+print "[*] Converting Markdown README to reStructuredText"
 rst = pypandoc.convert_file('README.md', 'rst')
 with open('README.rst', 'w+', encoding='utf-8') as f:
     f.write(rst)
-print "[*] Finished converting to README.rst (%s bytes)" % len(rst)
+print "[*] Finished converting to README.rst ({} bytes)".format(len(rst))
 
 yara_files = {}
 for root, dirnames, filenames in os.walk(rules_dir):
@@ -21,10 +21,14 @@ for root, dirnames, filenames in os.walk(rules_dir):
         yara_files[path] = path
 
 # print yara_files
+print "[*] Compiling {} Yara rule files".format(len(yara_files))
 rules = yara.compile(filepaths=yara_files)
-print "[*] Compiling %d Yara rules ..." % len(yara_files)
 rules.save(compiled_rules_path)
-print "[*] Saved rules to %s" % compiled_rules_path
+
+count = 0
+for _ in rules:
+    count += 1
+print "[*] Saved {} rules to {}".format(count, compiled_rules_path)
 
 # print "[*] Registering ..."
 # os.system("python setup.py register")
