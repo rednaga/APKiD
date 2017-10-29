ff21e8dc516e7e3533ab4dcec0a002067ac08701
Tim Strazzere <diff@sentinelone.com>
2017-10-29 15:41:09 -0700

    Add shebang and remove tty requirement.
    This will allow the script to be run in better automated fashions.

--- docker/apkid.sh
+++ docker/apkid.sh
@@ -1,7 +1,8 @@
+#!/bin/sh
 # This will simply take the argument passed to it,
 # parse the dirctory and bind it as a read-only mount point on the container
 # and pass in the filename as the argument to apkid
 #
 # This can easily be set to an alias and added to your .profile or whatever
 
-docker run --rm -v "`dirname $1`":/input:ro -it rednaga/apkid:v1  "`basename $1`";
+docker run --rm -v "`dirname $1`":/input:ro -i rednaga/apkid:v1  "`basename $1`";
