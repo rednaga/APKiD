0bab7d772c56bfe697bae8c82f530d7465020e0c
Caleb Fenton <calebjfenton@gmail.com>
2018-07-13 18:52:09 -0700

    Add rough testing framework

---
+++ tests/test_rules.py
@@ -0,0 +1,21 @@
+import warnings
+
+from apkid import rules
+
+
+def test_rules_compile():
+    rule_count = rules.compile()
+    assert rule_count > 0
+
+
+def test_lint_rules():
+    for r in rules.load():
+        if len(r.tags) == 0:
+            warnings.warn("rule has no tags: {}".format(r.identifier), stacklevel=0)
+
+        if 'description' not in r.meta:
+            warnings.warn("rule has no tags: {}".format(r.identifier), stacklevel=0)
+
+        if ('packer' in r.tags or 'protector' in r.tags or 'obfuscator' in r.tags) \
+                and 'sample' not in r.meta:
+            warnings.warn("rule has no reference sample: {}".format(r.identifier), stacklevel=0)
