0f5fac2d30b72d938bff47da3952192aaca9aa86
Caleb Fenton <calebjfenton@gmail.com>
2019-05-31 22:24:36 -0700

    Add license to new file

--- apkid/main.py
+++ apkid/main.py
@@ -1,3 +1,29 @@
+"""
+ Copyright (C) 2019  RedNaga. https://rednaga.io
+ All rights reserved. Contact: rednaga@protonmail.com
+
+
+ This file is part of APKiD
+
+
+ Commercial License Usage
+ ------------------------
+ Licensees holding valid commercial APKiD licenses may use this file
+ in accordance with the commercial license agreement provided with the
+ Software or, alternatively, in accordance with the terms contained in
+ a written agreement between you and RedNaga.
+
+
+ GNU General Public License Usage
+ --------------------------------
+ Alternatively, this file may be used under the terms of the GNU General
+ Public License version 3.0 as published by the Free Software Foundation
+ and appearing in the file LICENSE.GPL included in the packaging of this
+ file. Please visit http://www.gnu.org/copyleft/gpl.html and review the
+ information to ensure the GNU General Public License version 3.0
+ requirements will be met.
+"""
+
 import argparse
 
 from apkid.apkid import Scanner, Options
