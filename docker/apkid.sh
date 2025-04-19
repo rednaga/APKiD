b15b5229be2ca874ae97e5423b672b67da22ac97
Abhi <85984486+AbhiTheModder@users.noreply.github.com>
2025-04-19 21:28:47 +0000

    Update Dockerfile & script

--- docker/apkid.sh
+++ docker/apkid.sh
@@ -1,13 +1,33 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
 # This will simply take the argument passed to it,
 # parse the directory and bind it as a read-only mount point on the container
 # and pass in the filename as the argument to APKiD
 #
 # This can easily be set to an alias and added to your .profile or whatever
 
-# This assumes file target is last argument!
-TARGET="${@: -1}"
-INPUT_DIR=$(cd $(dirname "$TARGET") && pwd -P)
-INPUT_FILE=$(basename $TARGET)
+FILES=()
+ARGS=()
 
-docker run --rm --volume "$INPUT_DIR":/input:ro -i rednaga:apkid "/input/$INPUT_FILE" "${@:0:$#}";
+for arg in "$@"; do
+    if [[ -e "$arg" ]]; then
+        FILES+=("$arg")
+    else
+        ARGS+=("$arg")
+    fi
+done
+
+if [[ ${#FILES[@]} -eq 0 ]]; then
+    docker run --rm -i rednaga:apkid "${ARGS[@]}"
+    exit 0
+fi
+
+FIRST_FILE="${FILES[0]}"
+INPUT_DIR=$(cd "$(dirname "$FIRST_FILE")" && pwd -P)
+
+MAPPED_FILES=()
+for file in "${FILES[@]}"; do
+    MAPPED_FILES+=("/input/$(basename "$file")")
+done
+
+docker run --rm --volume "$INPUT_DIR":/input:ro -i rednaga:apkid "${ARGS[@]}" "${MAPPED_FILES[@]}"
